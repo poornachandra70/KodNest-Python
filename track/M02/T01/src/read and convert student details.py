7eca0de12a55a9e313babb84aef00f03123451c9
poornachandra <deepakjabin407@gmail.com>
2026-08-05 15:58:53 +0530

    Update division solution

---
+++ track/M02/T01/src/read and convert student details.py
@@ -0,0 +1,16 @@
+# Read and Convert Student Details
+# A student registration system receives the student’s name, age and course rating.
+
+
+# Read and convert the student details
+Student_name = input()
+Student_age = int(input())
+Course_rating = float(input())
+# Display the values
+print(f"Student: {Student_name}")
+print(f"Age: {Student_age}")
+print(f"Rating: {Course_rating}")
+# Display the data types
+print(type(Student_name))
+print(type(Student_age))
+print(type(Course_rating))
