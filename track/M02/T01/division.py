79fb9fc49afd3ec00adce0fe19996e5bde2e0fd8
poornachandra <deepakjabin407@gmail.com>
2026-08-05 15:05:14 +0530

    Update division solution

---
+++ track/M02/T01/division.py
@@ -0,0 +1,22 @@
+# Calculate Division and Floor Division
+# Python provides two different operators
+# for division:
+# / performs regular division and
+# produces a decimal result.
+# // performs floor division and
+# produces the whole-number
+# quotient for positive integers.
+# Write a Python program that reads two
+# positive integers and prints:
+# 1. The result of regular division.
+# 2. The result of floor division.
+
+
+ # CODE 
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+Sum = first_number + second_number
+print(f"Sum: {Sum}")
+
+
