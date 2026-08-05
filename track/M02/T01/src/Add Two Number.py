4dc3c6acb698c044dfa0020572f9cc6341b53191
poornachandra <deepakjabin407@gmail.com>
2026-08-05 16:39:13 +0530

    Add eligibility criteria script and Add Two Number script

---
+++ track/M02/T01/src/Add Two Number.py
@@ -0,0 +1,7 @@
+# Read the numbers
+first_number=int(input())
+second_number=int(input())
+# Calculate the sum
+sum = first_number + second_number
+# Display the result
+print(f"Sum: {sum}")
--- track/M02/T01/src/eligilbility crei.py
+++ track/M02/T01/src/eligilbility crei.py
@@ -3,11 +3,11 @@ Marks= int(input())
 Attendence= int(input())
 Project_completion= input()
 # Check the academic requirements
-if (Marks >=60) and (Attendence>=75) :
-if Project_completion == "yes" :
-print("Eligible")
-else :
-print("Not Eligible")
-else :
-print("Not Eligible")
+if (Marks >=60) and (Attendence>=75):
+    if Project_completion == "yes":
+        print("Eligible")
+    else:
+        print("Not Eligible")
+else:
+    print("Not Eligible")
 
