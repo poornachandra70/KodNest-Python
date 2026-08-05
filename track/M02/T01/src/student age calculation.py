a014aa99c8cb2dbef56fd47b9ceb3901e7cb8bd6
poornachandra <deepakjabin407@gmail.com>
2026-08-05 15:55:46 +0530

    Update division solution

---
+++ track/M02/T01/src/student age calculation.py
@@ -0,0 +1,8 @@
+# Fix the Student Age Calculation
+# A student’s current age has been stored as text.
+
+
+current_age = "21"
+current_age = int(current_age)
+next_age = current_age + 1
+print(f"Age next year: {next_age}")
